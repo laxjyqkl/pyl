8c1ae362451f785c924defbc49af48d3fe746e77
root <laxjyqkl@yahoo.com>
2018-10-09 22:47:35 -0400

    Exercise of function

---
+++ 函数1.py
@@ -0,0 +1,13 @@
+def printinfo():
+    print("hello ,friend?")
+    print("\"hello ,friend.\"")
+    print("Maybe i should give you a name")
+
+def printinfo1():
+    print('but that\'s a slippery slope')
+
+def printinfo2():
+    print('you\'re only in my head')
+printinfo()
+printinfo1()
+printinfo2()
